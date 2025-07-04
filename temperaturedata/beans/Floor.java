5edfd21f339a163d0b4b6b107c6066f0813cb552
nasreenbanu <nasreenbanua24it@vetias.ac.in>
2025-07-04 09:56:52 +0530

    floor

--- temperaturedata/beans/Floor.java
+++ temperaturedata/beans/Floor.java
@@ -1,5 +1,18 @@
 public class Floor{
     private String name;
     private int number;
-      
+    public void name(String name) {
+          name = name;
+       }
+
+       public String getname() {
+         return name;
+       }   
+    public void number(int number) {
+          number = number;
+       }
+
+       public int getnumber() {
+         return number;
+       }   
 }
