ded1b21127ca765d7f3fbe2d8e260b34dcff1431
nasreenbanu <nasreenbanua24it@vetias.ac.in>
2025-07-04 10:01:35 +0530

    sensor

--- temperaturedata/beans/sensor.java
+++ temperaturedata/beans/sensor.java
@@ -2,5 +2,25 @@ public class sensor {
     private String name;
     private int id;
     private String temperaturedata;
+    public void name(String name) {
+          name = name;
+       }
 
+       public String getname() {
+         return name;
+       }   
+    public void id(int id) {
+          id = id;
+       }
+
+       public int id() {
+         return id;
+       }   
+    public void temperaturedata(int temperaturedata) {
+          temperaturedata = temperaturedata;
+       }
+
+       public String gettemperaturedata() {
+         return temperaturedata;
+       }      
 }
