fb24e8f55347da8e3b4acfd9ce0193f6ec6a47bc
nasreenbanu <nasreenbanua24it@vetias.ac.in>
2025-07-04 10:05:10 +0530

    zone

--- temperaturedata/beans/zone.java
+++ temperaturedata/beans/zone.java
@@ -2,5 +2,26 @@ public class zone {
     private String name;
     private int zoneid;
     private String type;
+    public void name(String name) {
+          name = name;
+       }
+
+       public String getname() {
+         return name;
+       }   
+    public void zoneid(int zoneid) {
+          zoneid = zoneid;
+       }
+
+       public int getzoneid() {
+         return zoneid;
+       }  
+    public void type(String type) {
+          type = type;
+       }
+
+       public String gettype() {
+         return type;
+       }          
 
 }
