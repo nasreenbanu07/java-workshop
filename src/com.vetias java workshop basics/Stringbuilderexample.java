f3059cc3c91c3e422eeb913262a9880aad6875b1
nasreenbanu <nasreenbanua24it@vetias.ac.in>
2025-07-07 15:59:56 +0530

    Builder

---
+++ src/com.vetias java workshop basics/Stringbuilderexample.java
@@ -0,0 +1,20 @@
+
+public class Stringbuilderexample {
+    public static void main(String[] args) {
+        StringBuilder sentence=new StringBuilder();
+        sentence.append(" java");
+        sentence.append(" is");
+        sentence.append(" the");
+        sentence.append(" most");
+        sentence.append(" used");
+        sentence.append(" language");
+        sentence.append(" for");
+        sentence.append(" enterprise");
+        sentence.append(" applications");
+        System.out.println(sentence.toString());
+        
+        }
+        
+    }
+
+
---
+++ src/com.vetias java workshop basics/array/Generatorname.java
@@ -0,0 +1,29 @@
+public class Generatorname {
+    public static void main(String[] args){
+        scanner sc = scanner(System.in);
+        System.out.println("enter your name:");
+        String name= sc.next();
+        System.out.println("enter the gender:");
+        String gender=sc.next();
+        System.out.println("enter the qualification:");
+        String qualification=sc.next();
+        if("male".equalsIgnorecase(gender)){
+            System.out.println="mr. ";
+        }
+        else if ("female".equalIgnorecase(gender)){
+             System.out.println("Ms:"+name+","+qualification:"+qualification");
+        }
+        else{
+            System.out.println("invalid gender:");
+        }
+
+        
+        
+        }
+
+
+    }
+        
+    
+
+
