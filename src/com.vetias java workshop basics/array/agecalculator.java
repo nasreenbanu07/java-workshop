3c9a8966c5ffb65c0007b1e5e3fd5876d0a61aef
nasreenbanu <nasreenbanua24it@vetias.ac.in>
2025-07-08 11:18:43 +0530

    age

--- src/com.vetias java workshop basics/array/agecalculator.java
+++ src/com.vetias java workshop basics/array/agecalculator.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-    import java.time.LocalDate;
-       import java.text.ParseException;
+import java.time.LocalDate;
 public class agecalculator {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        System.out.println("enter your yearofbirth:");
-        int yearofbirth = inputScanner.nextInt();
-        int currentyear = LocalDate.now().getYear();
-        int age = currentyear-yearofbirth;
-        System.out.println("your age is:"+age);
+        //System.out.println("enter your yearofbirth:");
+        //int yearofbirth = inputScanner.nextInt();
+        //int currentyear = LocalDate.now().getYear();
+        //int age = currentyear-yearofbirth;
+        //System.out.println("your age is:"+age);
 // comment
 System.out.println("enter your yearofbirth:");
 String dateofbirth = inputScanner.nextLine();
