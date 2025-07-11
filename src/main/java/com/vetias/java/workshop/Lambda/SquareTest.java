d03ff1ce1feb4044e1ea15f1c204b5a9cdf27ada
nasreenbanu <nasreenbanua24it@vetias.ac.in>
2025-07-11 14:45:03 +0530

    square

--- src/main/java/com/vetias/java/workshop/Lambda/SquareTest.java
+++ src/main/java/com/vetias/java/workshop/Lambda/SquareTest.java
@@ -1,11 +1,17 @@
 package com.vetias.java.workshop.Lambda;
 
 public class SquareTest {
-    private static final char[] square;
+    private static final char[] square = new char[10]; // Example initialization
     private static final String Assert = null;
 
     public static void main(String[] args) { 
-        Square.square=(int x) {
+        Object x;
+         x = new Object() {
+            int findSquare(int n) {
+                return n * n;
+            }
+         };
+        Square.square=(x) {
             System.out.println(square);
             Assert.assertEquals(36, square.findSquare(6));
         
